533a511cda262d7fd8914a22103f0282e005ea77
jiadchen <jiadchen@redhat.com>
2023-04-03 13:29:13 +0900

    修正

--- api/laravel10.x/tests/api/UserCreateApiTest.php
+++ api/laravel10.x/tests/api/UserCreateApiTest.php
@@ -13,21 +13,6 @@ class UserCreateApiTest extends TestCase
 {
     use DatabaseMigrations; // メモリ以外のDBはRefreshDatabaseが効かない問題：https://laracasts.com/discuss/channels/testing/refreshdatabase-trait-doesnt-refresh-database
 
-    protected WireMock $wiremock;
-
-    public function setUp(): void // voidをつけないとIlluminate\Foundation\Testing\TestCase の setUpにならずエラー
-    {
-        parent::setUp();
-        // WireMockサーバーのセットアップ
-        $this->wiremock = WireMock::create('127.0.0.1', 3000);
-        $this->wiremock->reset();
-    }
-
-    public function tearDown(): void // voidをつけないとIlluminate\Foundation\Testing\TestCase の setUpにならずエラー
-    {
-        $this->wiremock->reset();
-        parent::tearDown();
-    }
     /**
      * Test if a user can be created successfully and the expected response is returned
      */
@@ -58,64 +43,5 @@ class UserCreateApiTest extends TestCase
         // Assert
         $this->assertSame($actualStatus, $expectedStatus);
         $this->assertEquals($actualUser, $expectedUser);
-    }
-
-    public function test_すべてのユーザ情報を取得できること()
-    {
-        // Arrange
-        $expectedStatus = 200;
-        $expectedUsers = json_encode([
-            [
-                'name' => 'John Doe',
-                'email' => 'john@example.com',
-                'phoneNumber' => '08011112222'
-            ],
-            [
-                'name' => 'Jane Doe',
-                'email' => 'jane@example.com',
-                'phoneNumber' => '08011113333'
-            ]
-        ]);
-        // Database準備
-        $client = new Client();
-        EloquentUser::create([
-            'name' => 'John Doe',
-            'email' => 'john@example.com',
-        ]);
-        EloquentUser::create([
-            'name' => 'Jane Doe',
-            'email' => 'jane@example.com',
-        ]);
-
-        // John Doeのプライバシー情報を返すマッピング
-        $this->wiremock->stubFor(
-            WireMock::get(WireMock::urlPathEqualTo('/api/v1/privacy'))
-                ->withQueryParam('email', WireMock::equalTo('john@example.com'))
-                ->willReturn(
-                    WireMock::aResponse()
-                        ->withStatus(200)
-                        ->withBody('{ "address": "Tokyo", "phone_number": "08011112222" }')
-                )
-        );
-        // Jane Doeのプライバシー情報を返すマッピング
-        $this->wiremock->stubFor(
-            WireMock::get(WireMock::urlPathEqualTo('/api/v1/privacy'))
-                ->withQueryParam('email', WireMock::equalTo('jane@example.com'))
-                ->willReturn(
-                    WireMock::aResponse()
-                        ->withStatus(200)
-                        ->withBody('{ "address": "Tokyo", "phone_number": "08011113333" }')
-                )
-        );
-
-        // Act
-        $client = new Client();
-        $response = $client->get('http://localhost:8000/api/v1/users');
-        $actualStatus = $response->getStatusCode();
-        $actualUsers = $response->getBody()->getContents();
-
-        // Assert
-        $this->assertSame($expectedStatus, $actualStatus);
-        $this->assertEquals($expectedUsers, $actualUsers);
     }
 }
