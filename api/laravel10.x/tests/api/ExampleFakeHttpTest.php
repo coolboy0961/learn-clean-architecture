5f6b4659152d78841cf878bde70943e45d44f590
jiadchen <jiadchen@redhat.com>
2023-04-12 11:34:45 +0900

    ミスの修正

--- api/laravel10.x/tests/api/ExampleFakeHttpTest.php
+++ api/laravel10.x/tests/api/ExampleFakeHttpTest.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Api;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleFakeHttpTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/api/v1/hello-world');
+  /**
+   * A basic test example.
+   */
+  public function test_the_application_returns_a_successful_response(): void
+  {
+    $response = $this->get('/api/v1/hello-world');
 
-        $response->assertStatus(200);
-    }
+    $response->assertStatus(200);
+  }
 }
