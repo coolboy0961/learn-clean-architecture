93c4a60d4dcb9578e3e0deae6dbf147003a2f024
jiadchen <jiadchen@redhat.com>
2023-04-12 13:08:48 +0900

    ミスの修正

--- api/laravel10.x/tests/api/ExampleRealHttpTest.php
+++ api/laravel10.x/tests/api/ExampleRealHttpTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Api;
 
 use Tests\TestCase;
 use Illuminate\Http\Request;
@@ -8,24 +8,24 @@ use GuzzleHttp\Client;
 
 class ExampleRealHttpTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        // Arrange
-        $expectedStatus = 200;
-        $expectedContent = ['message' => 'Hello World'];
+  /**
+   * A basic test example.
+   */
+  public function test_the_application_returns_a_successful_response(): void
+  {
+    // Arrange
+    $expectedStatus = 200;
+    $expectedContent = ['message' => 'Hello World'];
 
-        // Act
-        $client = new Client();
-        $response = $client->request('GET', 'http://localhost:8000/api/v1/hello-world');
-        $actualStatus = $response->getStatusCode();
-        $actualContent = json_decode($response->getBody()->getContents(), true);
+    // Act
+    $client = new Client();
+    $response = $client->request('GET', 'http://localhost:8000/api/v1/hello-world');
+    $actualStatus = $response->getStatusCode();
+    $actualContent = json_decode($response->getBody()->getContents(), true);
 
-        // Assert
-        $this->assertSame($actualStatus, $expectedStatus);
-        $this->assertEquals($actualContent, $expectedContent);
-        $this->assertSame('apitest', env("APP_ENV"));
-    }
+    // Assert
+    $this->assertSame($actualStatus, $expectedStatus);
+    $this->assertEquals($actualContent, $expectedContent);
+    $this->assertSame('apitest', env("APP_ENV"));
+  }
 }
