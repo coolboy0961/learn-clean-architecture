86d422ddae7f465095e42a8442b3c0e0a3d0e0ef
jiadchen <jiadchen@redhat.com>
2024-10-29 15:04:18 +0900

    nuxt3のサンプル作成

--- ui/nuxt3/nuxt.config.ts
+++ ui/nuxt3/nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  // spaモードを有効にする
+  ssr: false,
+  // その他の設定
+  app: {
+    // headタグの設定
+    head: {
+      title: "My SPA Site",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
+});
